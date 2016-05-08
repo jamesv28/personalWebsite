b01ede19e761e7c5113272eca98469b20a19e031
James Volmert <james.volmert@gmail.com>
2016-05-07 22:15:07 -0600

    finally got the slanted div to work. need to change header text and also keep working on the div themselvess. started back from  scratch with content and need to put it all together tomorrow

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@
 $(function() {
     var wheight = $(window).height(); //get height of the window
 
-    $('#feeling').css('height', wheight);
+    $('#indexHead').css('height', wheight);
 
     $(window).resize(function() {
         var wheight = $(window).height(); //get height of the window
-        $('#feeling').css('height', wheight);
+        $('#indexHead').css('height', wheight);
     }); //on resize
 
 }); // end of jQuery script
