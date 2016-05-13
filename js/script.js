7dd58be919bea451993ce08416ecb463518e8c4c
James Volmert <james.volmert@gmail.com>
2016-05-12 19:02:42 -0600

    Got animated scrolling and styling to work

--- js/script.js
+++ js/script.js
@@ -9,4 +9,20 @@ $(function() {
         $('#indexHead').css('height', wheight);
     }); //on resize
 
+    /* animated scrolling */
+    var topoffset = 40;
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top-topoffset
+                }, 1000);
+                return false;
+            } // target.length
+        } //location hostname
+    }); //on click
+
+
 }); // end of jQuery script
