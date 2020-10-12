235fcf4f7599bdaa87eca6b433b074d2d829c440
James Volmert <james.volmert@gmail.com>
2020-10-12 14:17:49 -0600

    almost finished with site

--- js/script.js
+++ js/script.js
@@ -1,48 +1,6 @@
+new WOW().init();
 
 $(function() {
-    var wheight = $(window).height(); //get height of the window
-
-    /* animated scrolling */
-    var topoffset = 40;
-    $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-            if (target.length) {
-                $('html,body').animate({
-                    scrollTop: target.offset().top-topoffset
-                }, 1000);
-                return false;
-            } // target.length
-        } //location hostname
-    }); //on click
-
-    $('#slide01').animatedModal({
-        animatedIn: 'lightSpeedIn',
-        animatedOut:'bounceOutDown'
-
-    });
-    //typing
-    $('.typing').typed({
-        strings: [' Developer',' Designer', 'n all around cool dude'],
-        typeSpeed: 10,
-        showCursor: true,
-        loop: true,
-        backDelay: 1000
-    });
-
-    $('.owl-carousel').owlCarousel({
-        animateOut: 'bounceOutRight',
-        animateIn: 'bounceInLeft',
-        items:1,
-        margin:30,
-        loop: true,
-        dots: true,
-        dotsEach: true,
-        autoplay: true,
-        stagePadding:30,
-        dotData: true,
-        smartSpeed:250
-    });
+    
 
 }); // end of jQuery script
